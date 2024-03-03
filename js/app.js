51b6ca45e270025982a22b021cdedf1291349496
Juboraj Sujon <mdsujon72@gmail.com>
2024-03-03 20:01:21 +0600

    define the letest post container and api data forEach loop with console

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
+const latestPostContainer = document.getElementById("latest-post-container");
+console.log(latestPostContainer);
 // latest Posts API handler
 
 const fetchDataLatestPost = async () => {
   const url = `https://openapi.programming-hero.com/api/retro-forum/latest-posts`;
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data);
+  data.forEach((item) => {
+    console.log(item);
+  });
 };
 
 fetchDataLatestPost();
