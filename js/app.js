97e86a68614717760d2f9143a9ccd7a69986efe0
Juboraj Sujon <mdsujon72@gmail.com>
2024-03-04 00:42:00 +0600

    Added all post function handler

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const latestPostContainer = document.getElementById("latest-post-container");
 const latestPostLoader = document.getElementById("latest-post-loader");
+const allPostContainer = document.getElementById("all-post-container");
+const allPostLoader = document.getElementById("all-post-loader");
 
 // latest Posts API handler
 const fetchDataLatestPost = async () => {
@@ -22,20 +24,20 @@ const fetchDataLatestPost = async () => {
   } finally {
     // Hide loader after at least 2 seconds
     setTimeout(() => {
-      document.getElementById("latest-post-loader").classList.add("hidden");
+      latestPostLoader.classList.add("hidden");
     }, 2000);
   }
 };
 // appendChild function for latest post container
 const appendLatestPosts = (data) => {
   data.forEach((item) => {
-    const article = createArticleElemet(item);
+    const article = createArticleElemetLatest(item);
     latestPostContainer.appendChild(article);
   });
 };
 
 //Create article tag handler for latest post container
-const createArticleElemet = (item) => {
+const createArticleElemetLatest = (item) => {
   //define article tag
   const article = document.createElement("article");
   // add classname inside article tag
@@ -75,5 +77,101 @@ const createArticleElemet = (item) => {
     `;
   return article;
 };
+
+// let's descuss left container API handler
+const fetchDataAllPost = async () => {
+  try {
+    //clear container
+    allPostContainer.innerHTML = "";
+
+    //Show loader for at least 2 second
+    allPostLoader.classList.remove("hidden");
+
+    // fetch data from api
+    const url = `https://openapi.programming-hero.com/api/retro-forum/posts`;
+    const res = await fetch(url);
+    const data = await res.json();
+    const postsData = data.posts;
+
+    // call the appendAllPosts handler
+    appendAllPosts(postsData);
+  } catch (error) {
+    console.log("Error from fetching all post api:", error);
+  } finally {
+    // Hide loader after at least 2 seconds
+    setTimeout(() => {
+      allPostLoader.classList.add("hidden");
+    }, 2000);
+  }
+};
+
+const appendAllPosts = (data) => {
+  data.forEach((item) => {
+    const article = createArticleElemetAll(item);
+    allPostContainer.appendChild(article);
+  });
+};
+
+const createArticleElemetAll = (item) => {
+  // check user isAcitve or not
+  isUserActive = item?.isActive || false;
+
+  //create an object for read complete list
+  const title = item?.title || "No title";
+  const postViewCount = item?.view_count || "No view";
+
+  //define article tag
+  const article = document.createElement("article");
+  // add classname inside article tag
+  article.className = `bg-[#F3F3F5] hover:bg-[#797DFC1A] p-5 md:p-7 rounded-lg sm:rounded-xl md:rounded-2xl shadow-md flex flex-col sm:flex-row gap-3 md:gap-5`;
+  //use template litarel for article content
+  article.innerHTML = `
+    <div class="indicator shrink-0">
+      <span class="indicator-item badge ${
+        isUserActive ? "bg-green-700" : "bg-red-700"
+      } p-1.5 h-3"></span>
+      <div
+        class="grid w-20 h-20 bg-white place-items-center rounded-xl">
+        <img src="${item?.image}" alt="${item?.author?.name}" />
+      </div>
+    </div>
+    <div class="text-[#12132D] font-inter grow">
+      <p class="text-sm font-semibold opacity-80 mb-3">
+        <span class="mr-4"># ${item?.category}</span
+        ><span>Author: ${item?.author?.name}</span>
+      </p>
+      <h3 class="text-xl mb-4 font-mulish">
+        ${item?.title}
+      </h3>
+      <p class="opacity-60 text-base mb-5">
+        ${item?.description}
+      </p>
+      <div
+        class="border-t-2 border-t-slate-400 border-dotted mb-5"></div>
+      <!-- social activities -->
+      <div class="flex items-center justify-between">
+        <div class="text-sm sm:text-base md:text-lg opacity-60">
+          <i class="fa-regular fa-message sm:mr-2"></i>
+          <span class="mr-2 sm:mr-4">${item?.comment_count}</span>
+          <i class="fa-regular fa-eye sm:mr-2"></i>
+          <span class="mr-2 sm:mr-4">${item?.view_count}</span>
+          <i class="fa-regular fa-clock sm:mr-2"></i>
+          <span class="mr-2 sm:mr-4">${item?.posted_time} min</span>
+        </div>
+        <div onclick="addToReadedList('${title}', '${postViewCount}')"
+          class="text-sm sm:text-base md:text-lg text-white px-2 py-0.5 bg-green-600 rounded-full">
+          <i class="fa-solid fa-envelope-open"></i>
+        </div>
+      </div>
+    </div>
+    `;
+  return article;
+};
+
+const addToReadedList = (title, postViewCount) => {
+  console.table([title, postViewCount]);
+};
+
+fetchDataAllPost();
 
 fetchDataLatestPost();
